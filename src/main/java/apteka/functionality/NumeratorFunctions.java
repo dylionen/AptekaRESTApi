752dabb8f445ff63fa345b36936889376542be4a
dylionen <41701026+dylionen@users.noreply.github.com>
2019-12-30 15:07:33 +0100

    Added created numerator function

--- src/main/java/apteka/functionality/NumeratorFunctions.java
+++ src/main/java/apteka/functionality/NumeratorFunctions.java
@@ -5,6 +5,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.w3c.dom.DocumentType;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -19,16 +20,11 @@ public class NumeratorFunctions {
         Transaction tx = session.beginTransaction();
 
         String returnValue;
-        Numerator numerator;
+        Numerator numerator = null;
+        numerator = getCurrentOrCreateNumerator(session, idLocalization, documentType);
 
 
-        List<Numerator> numeratorList = session.createQuery("from Numerator num where num.idLocalization.idLocalization = "
-                + idLocalization + " and num.idWHMType.id_whmtype = " + documentType).getResultList();
-
-        if (numeratorList.size() == 1) {
-            numerator = numeratorList.get(0);
-        } else {
-            numerator = null;
+        if (numerator == null) {
             return "error";
         }
 
@@ -42,6 +38,47 @@ public class NumeratorFunctions {
         session.close();
 
         return returnValue;
+    }
+
+    private static Numerator getCurrentOrCreateNumerator(Session session, int idLocalization, int documentType) {
+        Numerator numerator;
+        numerator = getCurrentNumerator(session, idLocalization, documentType);
+
+        if (numerator == null) {
+            createNewNumerator(idLocalization, documentType);
+            numerator = getCurrentNumerator(session, idLocalization, documentType);
+        }
+        return numerator;
+    }
+
+    private static Numerator getCurrentNumerator(Session session, int idLocalization, int documentType) {
+        List<Numerator> numeratorList = session.createQuery("from Numerator num where num.idLocalization.idLocalization = "
+                + idLocalization + " and num.idWHMType.id_whmtype = " + documentType).getResultList();
+        if (numeratorList.size() == 0)
+            return null;
+        else
+            return numeratorList.get(0);
+    }
+
+
+    private static void createNewNumerator(int idLocalization, int documentType) {
+        SessionFactory sessionFactory = SessionFactories.NewFactory();
+        Session session = sessionFactory.getCurrentSession();
+        Transaction tx = session.beginTransaction();
+
+        List<Localization> localizationList = session.createQuery("from Localization where idLocalization = "
+                + idLocalization).getResultList();
+        Localization localization = localizationList.get(0);
+
+        List<TypesWHM> typesWHMList = session.createQuery("from TypesWHM where id_whmtype = " + documentType).getResultList();
+        TypesWHM typesWHM = typesWHMList.get(0);
+
+        String description = localization.getAkronim() + " " + typesWHM.getAkronim();
+        Numerator numerator = new Numerator(0, description, true, typesWHM, 0, 0, localization);
+
+        session.save(numerator);
+        session.getTransaction().commit();
+        session.close();
     }
 
     private static void validateNumerator(Numerator numerator, Date date) {
